8a221bd0a876fc25b08613d8bd7e18aa5b760f62
Kai Staud <Kai-Staud@web.de>
2025-01-19 12:38:32 +0100

    Expand dictionary for CiA402 Status / Control word

---
+++ Core/Src/cia402/callbacks.c
@@ -0,0 +1,11 @@
+/*
+ * callbacks.c
+ *
+ *  Created on: Jan 19, 2025
+ *      Author: kai
+ */
+
+#include "callbacks.h"
+
+
+
---
+++ Core/Src/cia402/callbacks.h
@@ -0,0 +1,19 @@
+/*
+ * callbacks.h
+ *
+ *  Created on: Jan 19, 2025
+ *      Author: kai
+ */
+
+#ifndef SRC_CIA402_CALLBACKS_H_
+#define SRC_CIA402_CALLBACKS_H_
+
+#include <lely/co/nmt.h>
+#include <lely/co/sdev.h>
+#include <lely/co/sdo.h>
+#include <lely/co/time.h>
+//#include "co/co.h"
+
+
+
+#endif /* SRC_CIA402_CALLBACKS_H_ */
